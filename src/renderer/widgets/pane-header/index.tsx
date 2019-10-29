365b5ea993612676b141d9670e969e60fb5446d8
Anton Strogonoff <anton@strogonoff.name>
2019-10-29 16:41:17 +0800

    Split from ITU OB editor: initial version

---
+++ src/renderer/widgets/pane-header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './styles.scss';
+
+
+interface PaneHeaderProps {
+  loud?: boolean,
+  align?: 'left' | 'right',
+  className?: string,
+}
+export const PaneHeader: React.FC<PaneHeaderProps> = function (props) {
+  let alignmentClass: string;
+  if (props.align === 'left') {
+    alignmentClass = styles.paneHeaderAlignedLeft;
+  } else if (props.align === 'right') {
+    alignmentClass = styles.paneHeaderAlignedRight;
+  } else {
+    alignmentClass = '';
+  }
+
+  return (
+    <h2 className={`
+      ${styles.paneHeader}
+      ${alignmentClass}
+      ${props.className ? props.className : ''}
+      ${props.loud ? styles.paneHeaderLoud : ''}
+    `}>{props.children}</h2>
+  )
+};
