4c4b3bf732b04ac962014dd3731bd006e7e6b391
Anton Strogonoff <anton@strogonoff.name>
2019-11-11 01:15:07 +0800

    gitpkg

---
+++ renderer/widgets/pane-header/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+interface PaneHeaderProps {
+    loud?: boolean;
+    align?: 'left' | 'right';
+    className?: string;
+}
+export declare const PaneHeader: React.FC<PaneHeaderProps>;
+export {};
---
+++ storage/renderer/repository-configurator.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+interface RepositoryConfiguratorProps {
+    defaultUrl: string;
+    className: string;
+}
+export declare const RepositoryConfigurator: React.FC<RepositoryConfiguratorProps>;
+export {};
