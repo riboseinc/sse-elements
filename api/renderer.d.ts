46b5077f0af62774b1069f63ad294865b15fbb13
Anton Strogonoff <anton@strogonoff.name>
2019-11-11 03:28:25 +0800

    gitpkg

---
+++ api/renderer.d.ts
@@ -0,0 +1,4 @@
+import { TimeTravel } from '../time-travel/renderer';
+export declare function apiRequest<T>(request: string, ...args: string[]): Promise<T>;
+export declare function useWorkspace<T>(request: string, reducer: any, initData: T, ...args: any[]): TimeTravel;
+export declare function useWorkspaceRO<T>(request: string, initData: T, poll?: boolean): T;
