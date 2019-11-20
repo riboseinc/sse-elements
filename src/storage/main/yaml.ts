ce7f9a85ce06db58048b122a12e3c0d89b258a11
Anton Strogonoff <anton@strogonoff.name>
2019-11-20 13:39:42 +0800

    fix: Do not merge changes, write file from scratch

--- src/storage/main/yaml.ts
+++ src/storage/main/yaml.ts
@@ -28,24 +28,24 @@ export class YAMLStorage {
     return yaml.load(data, { schema: SCHEMA });
   }
 
-  private async loadIfExists(filePath: string): Promise<any> {
-    let fileExists: boolean;
-    let oldData: any;
+  // private async loadIfExists(filePath: string): Promise<any> {
+  //   let fileExists: boolean;
+  //   let oldData: any;
 
-    try {
-      fileExists = (await this.fs.stat(filePath)).isFile() === true;
-    } catch (e) {
-      fileExists = false;
-    }
+  //   try {
+  //     fileExists = (await this.fs.stat(filePath)).isFile() === true;
+  //   } catch (e) {
+  //     fileExists = false;
+  //   }
 
-    if (fileExists) {
-      oldData = await this.load(filePath);
-    } else {
-      oldData = {};
-    }
+  //   if (fileExists) {
+  //     oldData = await this.load(filePath);
+  //   } else {
+  //     oldData = {};
+  //   }
 
-    return oldData || {};
-  }
+  //   return oldData || {};
+  // }
 
   public async store(filePath: string, data: any): Promise<any> {
     this.debugLog(`SSE: YAMLStorage: Storing ${filePath}`)
@@ -59,33 +59,34 @@ export class YAMLStorage {
         // Merge new data into old data; this way if some YAML properties
         // are not supported we will not lose them after the update.
         // TODO: This should be optional
-        let newData: any;
-        let oldData: any;
-        let newContents: string;
+        // let newData: any;
+        // let oldData: any;
+        // let newContents: string;
 
-        try {
-          oldData = await this.loadIfExists(filePath);
-          this.debugLog(`SSE: YAMLStorage: Storing ${filePath}: Already existing data: ${oldData}`, 'silly');
-          newData = Object.assign(oldData, data);
-          this.debugLog(`SSE: YAMLStorage: Storing ${filePath}: Combined data to write: ${newData}`, 'silly');
-        } catch (e) {
-          log.error(`SSE: YAMLStorage: Failed to store ${filePath}`);
-          console.error("Bad input", filePath, oldData, data);
-          throw e;
-        }
+        // try {
+        //   oldData = await this.loadIfExists(filePath);
+        //   this.debugLog(`SSE: YAMLStorage: Storing ${filePath}: Already existing data: ${oldData}`, 'silly');
+        //   newData = Object.assign(oldData, data);
+        //   this.debugLog(`SSE: YAMLStorage: Storing ${filePath}: Combined data to write: ${newData}`, 'silly');
+        // } catch (e) {
+        //   log.error(`SSE: YAMLStorage: Failed to store ${filePath}`);
+        //   console.error("Bad input", filePath, oldData, data);
+        //   throw e;
+        // }
 
         // console.debug(`Dumping contents for ${filePath} from ${data}`);
         // console.debug(oldData);
 
+        let newContents: string;
         try {
-          newContents = yaml.dump(newData, {
+          newContents = yaml.dump(data, {
             schema: SCHEMA,
             noRefs: true,
             noCompatMode: true,
           });
         } catch (e) {
           log.error(`SSE: YAMLStorage: Failed to dump ${filePath}: ${JSON.stringify(data)}`);
-          console.error(`Failed to save ${filePath} with ${JSON.stringify(newData)}`, e);
+          console.error(`Failed to save ${filePath} with ${JSON.stringify(data)}`, e);
           return;
         }
 
