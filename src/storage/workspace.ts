b584d864fc69179ea395e30aaa14af53205cbac6
Anton Strogonoff <anton@strogonoff.name>
2019-11-25 07:40:57 +0800

    chore: Document [legacy] workspace types

--- src/storage/workspace.ts
+++ src/storage/workspace.ts
@@ -1,4 +1,11 @@
 import { Index, IndexableObject } from './query';
 
 export type Workspace = { [indexName: string]: Index<IndexableObject> }
+/* Workspace is the collection of indexes that the application operates on.
+   There’s only one workspace.
+   This is the abstract workspace, the app provides a workspace
+   more precisely typed deriving from this type. */
 
+
+export type EmptyPartialWorkspace<W extends Workspace> = { [K in keyof W]?: {} };
+/* Represents an empty partial workspace, used only during app initialization. */
