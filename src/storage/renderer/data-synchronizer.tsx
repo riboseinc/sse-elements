511cba58b25ac743f82238cab288888b29fa909b
Anton Strogonoff <anton@strogonoff.name>
2019-12-11 08:37:08 +0900

    feat(git): Store username, author in settings
    BREAKING

--- src/storage/renderer/data-synchronizer.tsx
+++ src/storage/renderer/data-synchronizer.tsx
@@ -1,21 +1,12 @@
 import { remote, ipcRenderer } from 'electron';
 import { clipboard } from 'electron';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { H4, Card, Label, InputGroup, FormGroup, Callout, Button } from '@blueprintjs/core';
 
 import { useSetting } from '../../settings/renderer';
-import { request } from '../../api/renderer';
 
 import styles from './data-synchronizer.scss';
-
-
-type RepoConfig = {
-  originURL: string | null | undefined,
-  name: string | null | undefined,
-  email: string | null | undefined,
-  username: string | null | undefined,
-};
 
 
 interface DataSynchronizerProps {
@@ -23,19 +14,12 @@ interface DataSynchronizerProps {
   inPreLaunchSetup: boolean,
 }
 export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ upstreamURL, inPreLaunchSetup }) {
-  const [username, setUsername] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const [repoCfg, updateRepoCfg] = useState({
-    originURL: undefined,
-    name: undefined,
-    email: undefined,
-    username: undefined,
-  } as RepoConfig);
-
   const url = useSetting<string>('gitRepoUrl', '');
+  const name = useSetting<string>('gitAuthorName', '');
+  const email = useSetting<string>('gitAuthorEmail', '');
+  const username = useSetting<string>('gitUsername', '');
 
   const usingUpstream = url.value && url.value.trim() === upstreamURL.trim();
   let urlIsValid: boolean;
@@ -50,38 +34,28 @@ export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ ups
     urlIsValid = false;
   }
 
-  useEffect(() => {
-    fetchRepoConfig();
-  }, []);
-
-  useEffect(() => {
-    if (name.trim() === '' && repoCfg.name) { setName(repoCfg.name); }
-    if (email.trim() === '' && repoCfg.email) { setEmail(repoCfg.email); }
-    if (username.trim() === '' && repoCfg.username) { setUsername(repoCfg.username); }
-  }, [name, email, username, JSON.stringify(repoCfg)]);
-
   const complete = (
     urlIsValid &&
-    name.trim() != '' &&
-    email.trim() != '' &&
-    username.trim() != '');
-
-  async function updateGitConfigAndClose() {
-    await request<{ success: true }>('git-config-set', { name, email, username });
-    await closeWindow();
-  }
+    name.value && name.value.trim() != '' &&
+    email.value && email.value.trim() != '' &&
+    username.value && username.value.trim() != '');
 
   async function handleSaveAndClose() {
     setLoading(true);
+
+    await name.commit();
+    await email.commit();
+    await username.commit();
 
     // In pre launch we can modify URL, but updating Git config requires waiting for the app to load,
     // initializing the repo
     if (inPreLaunchSetup) {
       await url.commit();
-      ipcRenderer.on('app-loaded', updateGitConfigAndClose);
-    } else {
-      await updateGitConfigAndClose();
     }
+
+    setTimeout(() => {
+      remote.getCurrentWindow().close();
+    }, 2000);
   }
 
   async function handleResetURL() {
@@ -92,15 +66,6 @@ export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ ups
 
   async function copyUpstreamRepoURL() {
     clipboard.writeText(upstreamURL);
-  }
-
-  async function fetchRepoConfig() {
-    const repoCfg = await request<RepoConfig>('git-config-get');
-    updateRepoCfg(repoCfg);
-  }
-
-  async function closeWindow() {
-    await remote.getCurrentWindow().close();
   }
 
   return (
@@ -152,10 +117,10 @@ export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ ups
           <Label key="authorName">
             Author name
             <InputGroup
-              value={name}
+              value={name.value}
               type="text"
               onChange={(evt: React.FormEvent<HTMLElement>) => {
-                setName((evt.target as HTMLInputElement).value as string);
+                name.set((evt.target as HTMLInputElement).value as string);
               }}
             />
           </Label>
@@ -163,10 +128,10 @@ export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ ups
           <Label key="authorEmail">
             Author email
             <InputGroup
-              value={email}
+              value={email.value}
               type="email"
               onChange={(evt: React.FormEvent<HTMLElement>) => {
-                setEmail((evt.target as HTMLInputElement).value as string);
+                email.set((evt.target as HTMLInputElement).value as string);
               }}
             />
           </Label>
@@ -174,10 +139,10 @@ export const DataSynchronizer: React.FC<DataSynchronizerProps> = function ({ ups
           <Label key="username">
             Username
             <InputGroup
-              value={username}
+              value={username.value}
               type="text"
               onChange={(evt: React.FormEvent<HTMLElement>) => {
-                setUsername((evt.target as HTMLInputElement).value as string);
+                username.set((evt.target as HTMLInputElement).value as string);
               }}
             />
           </Label>
