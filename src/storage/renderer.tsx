b2ee29352caf3a7ee3c8a9f1aebc5611d4ee4376
Anton Strogonoff <anton@strogonoff.name>
2019-12-11 08:37:23 +0900

    feat: Add StorageContextSpec, ModifiedObjectStatus
    types.

--- src/storage/renderer.tsx
+++ src/storage/renderer.tsx
@@ -8,3 +8,20 @@ export type RendererStorage<S extends Storage> = {
 /* Has the same keys as Storage,
    but each content type identifier is assigned an ID-based index
    of that content type’s objects. */
+
+
+// Of the form: { objType1: [id1, id2], objType2: [id3, id4] }
+export type ModifiedObjectStatus<R extends RendererStorage<any>> = {
+  [K in keyof R]: (string | number)[]
+}
+
+
+export interface StorageContextSpec<R extends RendererStorage<any>> {
+  // Snapshot of all objects, per type
+  current: R,
+  refresh(): Promise<void>,
+
+  // Snapshot of modified object IDs, per type
+  modified: ModifiedObjectStatus<R>,
+  refreshModified(hasLocalChanges?: boolean): Promise<void>,
+}
