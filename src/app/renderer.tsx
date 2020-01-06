346745ee56f85bfd0153210ddfdaaf6872d684ef
Anton Strogonoff <anton@strogonoff.name>
2020-01-06 08:28:04 +0800

    docs: Document renderer initialization flow

--- src/app/renderer.tsx
+++ src/app/renderer.tsx
@@ -18,6 +18,8 @@ interface AppRenderer {
 }
 
 
+// Render application screen in a new window,
+// with given window component and (if applicable) any parameters.
 export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>>(config: C): Promise<AppRenderer> => {
 
   // electron-webpack guarantees presence of #app in index.html it bundles
@@ -26,16 +28,28 @@ export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>
   // Add a class allowing platform-specific styling
   document.documentElement.classList.add(`platform--${process.platform}`);
 
+  // Get all params passed to the window via GET query string
   const searchParams = new URLSearchParams(window.location.search);
+
+  // Prepare getter for requested top-level window UI React component
   const componentId = searchParams.get('c');
   const componentImporter = componentId ? config.windowComponents[componentId] : null;
 
   const App: React.FC<{}> = function ({ children }) {
+    /* Top-level abstract component. Renders the requested window UI component
+       wrapped in context processors (potentially more later). */
+
+    // Top-level window UI component(s) will be passed as children
     var result = children;
 
+    // Wrap UI component in any context providers configured by app developer
     for (const ContextProvider of Object.values(config.contextProviders)) {
       result = <ContextProvider>{result}</ContextProvider>;
     }
+
+    // Configure localization/translation context provider
+    // (currently hard-coded, not user-configurable),
+    // and render the total result wrapped in that
 
     const [langConfig, setLangConfig] = useState({
       available: config.app.languages.available,
@@ -52,6 +66,9 @@ export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>
       </LangConfigContext.Provider>
     );
   };
+
+  // Fetch top-level UI component class and render it.
+  // Show loading indicator while it’s being fetched.
 
   if (componentImporter) {
     ReactDOM.render(<Spinner />, appRoot);
